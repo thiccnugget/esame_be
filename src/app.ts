c4213627652016b7d4904777baa3eda107158f08
thiccnugget <samuelecammaraa@gmail.com>
2023-04-20 14:10:48 +0200

    fixed typo

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,19 @@ export const app = express();
 import mongoose from "mongoose";
 import auth from "./routes/auth";
 
-import products from "./routes/events";
+import events from "./routes/events";
 
 app.use(express.json());
 
 app.use("/v1/auth", auth);
-app.use("/v1/events", products);
+app.use("/v1/events", events);
 
 const serverPort = process.env.PORT || 3000;
 app.listen(serverPort, async () => {
   console.log(`Server is running on port ${serverPort}, using "${process.env.DB}" database`);
   await mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB}`);
 });
+
+
 
 export default app;
